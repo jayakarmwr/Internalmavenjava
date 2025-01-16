bdcfd57bf99a2e6a5bbf29def6f9f4f465ed5e16
jayakarmwr <jayakarmwr@gmail.com>
2025-01-16 17:25:45 +0530

    first commit

---
+++ src/main/java/com/app/demo/Internaljavamaven/App.java
@@ -0,0 +1,13 @@
+package com.app.demo.Internaljavamaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
